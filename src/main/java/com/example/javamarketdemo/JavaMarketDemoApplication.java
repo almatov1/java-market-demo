e4331077f5f85ec40a123b0c6352acb36f2ddf93
almatov1 <almatovalshin@gmail.com>
2023-08-01 12:51:43 +0500

    er

---
+++ src/main/java/com/example/javamarketdemo/JavaMarketDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.javamarketdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaMarketDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaMarketDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/javamarketdemo/JavaMarketDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javamarketdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaMarketDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
